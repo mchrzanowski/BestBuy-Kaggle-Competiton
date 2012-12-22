87c2a4cb174dc35737df78b70a02564336f74429
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-12-22 13:18:30 -0700

    create pickled dir if it's not already there

--- src/constants.py
+++ src/constants.py
@@ -13,6 +13,10 @@ TESTING_PREDICTIONS_FILE = os.path.join(ETC_DIR, "predictions.csv")
 PERSONAL_WORD_DICTIONARY_FILE = os.path.join(ETC_DIR, "product_dictionary.txt")
 
 PICKLING_DIR = os.path.join(BASE_DIR, "../pickled")
+
+if not os.path.exists(PICKLING_DIR):
+    os.makedirs(PICKLING_DIR)
+
 CLASSIFIER_PICKLING_FILE = os.path.join(PICKLING_DIR, "classifier")
 LABEL_TO_SKU_MAPPING_PICKLING_FILE = os.path.join(PICKLING_DIR, "label_to_sku_mapping")
 TRANSFORMER_PICKLING_FILE = os.path.join(PICKLING_DIR, "tfidf_transformer")
